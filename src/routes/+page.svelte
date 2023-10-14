256807d96cb858dcf456162c6200fd4824258710
Giuliano <giulianoverdone@gmail.com>
2023-10-14 00:25:43 -0400

    push from local repo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+import type { PageData } from "./$types";
+import { generations } from "./generations";
+
+export let data: PageData;
+
+</script>
+
+<div class="monsters">
+    {#each data.monsters as monster (monster.id)}
+        <img src="{monster.image}" alt="{monster.name}" srcset="">
+        <div class="monster">{monster.id}: {monster.name}</div>
+    {/each}
+</div>
+
+{#each generations as generation (generation.id)}
+    <h1>{generation.name}</h1>
+    <p>Games: {generation.games.join(', ')}</p>
+    <p>Main Region: {generation.main_region}</p>
+{/each}
+
+<style>
+.monsters {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+.monster {
+    height: 100px;
+    width: 100px;
+    border: 1px solid black;
+    margin: 5px;
+    padding: 10px;
+}
+</style>
